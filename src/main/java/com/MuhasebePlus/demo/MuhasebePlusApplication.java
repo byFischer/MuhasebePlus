0248cbc58bef553d98a8957cd5745efe7c10c4b6
Emre <115411304+byFischer@users.noreply.github.com>
2026-03-26 15:47:27 +0300

    Initial backend project structure

---
+++ src/main/java/com/MuhasebePlus/demo/MuhasebePlusApplication.java
@@ -0,0 +1,13 @@
+package com.MuhasebePlus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuhasebePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuhasebePlusApplication.class, args);
+	}
+
+}
