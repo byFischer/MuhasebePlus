b7991b1ce54844d36dda918e5bcd759bc412cda1
Emre <115411304+byFischer@users.noreply.github.com>
2026-03-29 19:12:39 +0300

    customer equalsandhascode eklendi

--- src/main/java/com/MuhasebePlus/demo/customer/entity/Customer.java
+++ src/main/java/com/MuhasebePlus/demo/customer/entity/Customer.java
@@ -4,6 +4,7 @@ import com.MuhasebePlus.demo.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "customer")
+@EqualsAndHashCode(callSuper = true)
 public class Customer extends BaseEntity {
 
     @Id
