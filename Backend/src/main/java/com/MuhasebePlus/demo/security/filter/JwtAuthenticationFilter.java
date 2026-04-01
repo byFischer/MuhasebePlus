980609bfb99099b7550b0be769db8b0333dd362f
Emre <115411304+byFischer@users.noreply.github.com>
2026-04-02 00:04:28 +0300

    security guncellemeleri yapildi

--- Backend/src/main/java/com/MuhasebePlus/demo/security/filter/JwtAuthenticationFilter.java
+++ Backend/src/main/java/com/MuhasebePlus/demo/security/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,19 @@
 package com.MuhasebePlus.demo.security.filter;
 
-public class JwtAuthenticationFilter {
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    // JWT doğrulama işlemleri burada yapılacaktır.
+   @Override
+        protected void doFilterInternal(HttpServletRequest request, 
+            HttpServletResponse response, 
+            FilterChain filterChain)
+        throws ServletException, IOException {
+            filterChain.doFilter(request, response);
+}
 }
--- Backend/src/main/java/com/MuhasebePlus/demo/security/util/JwtUtil.java
+++ Backend/src/main/java/com/MuhasebePlus/demo/security/util/JwtUtil.java
@@ -1,5 +1,62 @@
 package com.MuhasebePlus.demo.security.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import com.MuhasebePlus.demo.user.entity.User;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
 public class JwtUtil {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = secret.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    
+        
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        Claims claims = Jwts.parser().verifyWith(getSigninKey()).build().parseSignedClaims(token).getPayload();
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+    User user = (User) userDetails;
+    return Jwts.builder()
+            .subject(user.getUsername())
+            .claim("userId", user.getUserId())
+            .claim("role", user.getRole())
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + expiration))
+            .signWith(getSigninKey())
+            .compact();
+}
+    
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expirationDate = extractClaim(token, Claims::getExpiration);
+        return expirationDate.before(new Date());
+    }
 }
