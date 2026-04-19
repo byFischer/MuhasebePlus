eacdfb6bab87684fcb9e050a3ef5374230c70c58
Emre <115411304+byFischer@users.noreply.github.com>
2026-04-20 00:38:03 +0300

    v8 - Fix customer note column types

--- Backend/src/main/resources/db/migration/V8__fix_customer_note_column_types.sql
+++ Backend/src/main/resources/db/migration/V8__fix_customer_note_column_types.sql
@@ -1,5 +1,10 @@
 ALTER TABLE customer_note
+    ALTER COLUMN note_id TYPE BIGINT;
+
+ALTER TABLE customer_note
     ALTER COLUMN customer_id TYPE BIGINT;
+
+ALTER SEQUENCE customer_note_note_id_seq AS BIGINT;
 
 ALTER TABLE customer_note
     ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP;
